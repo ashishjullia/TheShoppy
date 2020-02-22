353ac47e396b737909027524b020aaa22fada58a
Ashish Jullia <ashishjullia19@gmail.com>
2020-02-22 18:15:52 -0500

    Task 3.1, 3.2, 3.3 has been completed

--- app/src/main/java/com/example/theshoppy/MainActivity.java
+++ app/src/main/java/com/example/theshoppy/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.theshoppy;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    String CountryList[] = { "Canada","India","USA", "Rusia", "Australia", "UK", "Brazil",
+                            "Italy", "Cuba", "Africa" , "Germany"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
+
+        AutoCompleteTextView autoCountries = (AutoCompleteTextView)findViewById(R.id.autoListCountries);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, CountryList);
+        autoCountries.setAdapter(adapter1);
+
+
     }
 }
