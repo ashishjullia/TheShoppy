50f5d443e3acbd1fbd016a6675159734068a9a81
Ashish Jullia <ashishjullia19@gmail.com>
2020-02-22 19:39:48 -0500

    Task 3.4 has been completed, added the date picker functionality

--- app/src/main/java/com/example/theshoppy/MainActivity.java
+++ app/src/main/java/com/example/theshoppy/MainActivity.java
@@ -3,15 +3,29 @@ package com.example.theshoppy;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
 
     String ProvinceList[] = {"Ontario", "Quebec", "Alberta" ,"Saskatchewan", "Manitoba",
                                 "New Brunswick", "Nova Scotia"};
+    DatePickerDialog datePicker;
+    EditText edtDate;
+    Button btnShowDate;
+    TextView txtDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,26 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, ProvinceList);
         autoProvinces.setAdapter(adapter1);
 
+        edtDate = (EditText)findViewById(R.id.editDate);
+        edtDate.setInputType(InputType.TYPE_NULL);
 
+        edtDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cal = Calendar.getInstance();
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                int month = cal.get(Calendar.MONTH);
+                int year = cal.get(Calendar.YEAR);
+                // date picker dialog
+                datePicker = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                edtDate.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                datePicker.show();
+            }
+        });
     }
 }
